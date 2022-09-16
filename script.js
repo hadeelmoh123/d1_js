530cecb681ec8b099846b3f529be94abaaaebfcd
hadeelmoh123 <112503299+hadeelmoh123@users.noreply.github.com>
2022-09-16 17:58:44 +0300

    Initial commit

---
+++ script.js
@@ -0,0 +1,11 @@
+//  console.log(sum); instead of this do this
+
+// Declaring new variables
+let x = 10;
+let y = 12;
+
+//  This is a statement that calculates the sum of x and y
+let z = x + y;
+
+// Logging to console
+console.log(z);
